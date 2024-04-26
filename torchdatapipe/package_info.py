b9b17d12145ba936d816bb005b20e6d298ed5185
Andrey Yashkin <andreyyashkin@gmail.com>
2024-04-26 14:34:02 +0700

    Add package_info.py

--- torchdatapipe/package_info.py
+++ torchdatapipe/package_info.py
@@ -1,10 +1,10 @@
 MAJOR = 0
 MINOR = 0
-PATCH = 0
-PRE_RELEASE = ""
+PATCH = 1
+PRE_RELEASE =  ""
 
 # Use the following formatting: (major, minor, patch, pre-release)
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
 
-__shortversion__ = ".".join(map(str, VERSION[:3]))
-__version__ = ".".join(map(str, VERSION[:3])) + "".join(VERSION[3:])
+__shortversion__ = '.'.join(map(str, VERSION[:3]))
+__version__ = '.'.join(map(str, VERSION[:3])) + ''.join(VERSION[3:])
