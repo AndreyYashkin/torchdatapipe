8ae3ec528a1cdf671b808f5d04d00c25ee157988
Andrey Yashkin <andreyyashkin@gmail.com>
2024-10-19 17:30:55 +0700

    Add python requirements.

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setuptools.setup(
     # https://packaging.python.org/en/latest/single_source_version.html
     # version=__version__,
     packages=setuptools.find_packages(),
+    python_requires=">=3.9",
     install_requires=install_requires,
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
